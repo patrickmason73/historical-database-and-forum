2f790b00ed941a936ddfae1725130b2437085aca
Patrick Mason <patrickmason73@gmail.com>
2023-09-23 18:36:13 -0500

    header/navbar components

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,8 +1,29 @@
 import React from "react";
 
+const headerStyle = {
+    display: "grid",
+    placeItems: "center",
+    fontSize: "200%"
+}
+
+const secondHeaderStyle = {
+    display: "grid",
+    placeItems: "center",
+    fontSize: "150%",
+    backgroundColor: "#add8e6",
+}
 
 function Header() {
 
+    return (
+        <>
+        <p style={headerStyle}><strong>HISTORICAL EVENTS PROJECT</strong></p>
+
+        <p style={secondHeaderStyle}>Login To Get Started!</p>
+        <p style={secondHeaderStyle}>Look At Posts From Other Users! You Can Also Make Your Own.</p>
+        <p style={secondHeaderStyle}>Found Something Interesting? Spotted Misinfomration? Leave A Comment Below The Post With Your Thoughts.</p>
+        </>
+    )
 }
 
 export default Header
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,9 +1,61 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+const linkStyles = {
+    width: "150px",
+    padding: "12px",
+    margin: "0 6px 6px",
+    background: "blue",
+    textDecoration: "none",
+    color: "white",
+    alignItems: "center",
+  };
+
+  const navStyle = {
+    height: "100px",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  }
 
 function Navbar() {
-    
+    return (
+        <div >
+            <nav style={navStyle}>
+                <NavLink to="/"
+               
+                style={linkStyles}
+               
+                >
+                 Home
+                </NavLink>
+                
+                <NavLink to="/login"
+               
+                style={linkStyles}
+               
+                >
+                 Login
+                </NavLink>
+                
+                <NavLink to="/signup"
+              
+                style={linkStyles}
+               
+                >
+                 Sign up
+                </NavLink>
+                
+                <NavLink to="/posts/index"
+              
+              style={linkStyles}
+             
+              >
+                Posts
+              </NavLink>
+                </nav>
+        </div>
+    )
 }
 
 
