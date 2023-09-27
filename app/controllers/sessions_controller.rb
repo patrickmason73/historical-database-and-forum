ab7f7c4510d838b16d1144824d8002c6d8719664
Patrick Mason <patrickmason73@gmail.com>
2023-09-26 19:18:21 -0500

    fixed error in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ skip_before_action :authorize, only: :create
     def create
         user = User.find_by(username: params[:username])
         if user&.authenticate(params[:password])
-            session[user_id] = user.id
+            session[:user_id] = user.id
             render json: user
         else
             render json: { errors: ["Invalid username or password"] }, status: :unauthorized
